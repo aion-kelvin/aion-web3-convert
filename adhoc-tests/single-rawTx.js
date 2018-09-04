4b93bd8d5586eb32ec19a86596c6ef1ab73e54b8
aion-kelvin <kelvin@aion.network>
2018-09-04 14:48:43 -0400

    use mastery account on adhoc-tests/single-rawTx.js

--- adhoc-tests/single-rawTx.js
+++ adhoc-tests/single-rawTx.js
@@ -13,20 +13,16 @@ because it uses `unlockAccount` rpc call it needs the password
 */
 let accounts = [
   {
-    address: '0xa0202797a7aff86fec1a5d8b7cacea276de5bcfc2e8b14878c9ba48d7d5330a0',
-    password: 'account1',
-    privateKey: '0x6df86a106f599c78ab9b2ad593b2983038edf706a52b24bfa895b49066a7f2a03ddfb8596435b9530b5e635736c801c1403578b85e582d98dd7a322ddfb1e4c1'
+    address: '0xa0450c4333e72ed26552d7462c0b3669924eec816a219b3960d5b3f0b33f7444',
+    password: 'password',
+    privateKey: '0xc0a97b1d6d68f3aed19e1d460287efcfd9e93cd294dd86963e08ab24622e4fc6b7dfe3a9fd46fbd034687a6c2a3ddfd61896b8e7521402369383d6a52a591a5b'
   },
   {
-    address: '0xa09d19c066f341220d914414c42e5e796e7e4daf83a9abf4581b1670403bbd15',
-    password: 'account2',
-    privateKey: '0x29379825937c50f0e13453673c5083bcf9853ef69ebb129f2db106db4966cf5efe6820305cf2cd29719d7c924fc495542100d5f499d4b758205bee05dd33896b'
+    address: '0xa0b88269779d225510ca880ed742e445db0c70efb1ee3159b6d56479ae3501f9',
+    password: 'whatever',
+    privateKey: '0xwhatever'
   },
-  {
-    address: '0xa06f640ced8bd31eb9e191887adde74888e9ca31fd8545dae3ae896773ccbc4f',
-    password: 'account3',
-    privateKey: '0xe6edbf765e724684cacc2c7cc0c8bbac98c1c672b8701108c6e2c2cf25523dec085aa353d4ebd256c4601838714cac4dfd96feee86bc03777247a085b7220ee1'
-  }
+  
 ]
 
 let privateKey = accounts[0].privateKey;
